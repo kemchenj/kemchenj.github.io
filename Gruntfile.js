e8a093214e870ef7190369b6f1e545542b7d647d
Kem Chen <chenkem@gmail.com>
2022-05-03 11:36:14 +0800

    Replace gulp with grunt
    Compared to gulp, grunt is:
    
    - well maintained, less security problems
    - lighter, less code and dependencies

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    htmlmin: {
+      options: {
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true
+      },
+      files: {
+        expand: true,
+        cwd: 'public/',
+        src: ['**/*.html'],
+        dest: 'public/',
+      }
+    },
+    cssmin: {
+      files: {
+        expand: true,
+        cwd: 'public/',
+        src: ['**/*.css'],
+        dest: 'public/'
+      }
+    },
+    uglify: {
+      files: {
+        expand: true,
+        cwd: 'public/',
+        src: ['**/*.js'],
+        dest: 'public/',
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['htmlmin', 'cssmin', 'uglify']);
+};
